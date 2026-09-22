9b60eecac2c081a3b0e7105928d9bd956dad673a
Jvfmjamba <45114279+Jvfmjamba@users.noreply.github.com>
2026-09-21 23:55:37 -0300

    feat: BR-4 criar rota de logout

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from . import views
@@ -6,5 +7,6 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('cadastro/', views.cadastro_view, name='cadastro'),
     path('perfil/', views.perfil_view, name='perfil'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
 #declarando a rota de cada pagina
